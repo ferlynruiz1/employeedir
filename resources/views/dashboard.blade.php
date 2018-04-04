776722adf2458ba024e519aaea05cac986372ff1
John Manuel Derecho <jmanuel.derecho@gmail.com>
2018-04-05 04:20:04 +0800

    fix image2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -74,7 +74,7 @@ Dashboard
                     <ul class="timeline">
                         @foreach($new_hires as $employee)
                         <li>
-                            <div class="timeline-badge"><img src="{{ $employee->profile_img }}" class="img-circle" alt="" style="width: 50px; height: 50px; margin-top: -10px; box-shadow: 1px 1px 10px 7px #fff;"></div>
+                            <div class="timeline-badge"><img src="{{ $employee->profile_img }}" class="img-circle" alt="" style="width: 50px; margin-top: -10px; box-shadow: 1px 1px 10px 7px #fff;"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4 class="timeline-title"><a href="employee_info/{{$employee->id}}" target="_blank"> {{ $employee->fullname() }}</a></h4>
@@ -114,7 +114,7 @@ Dashboard
                         <div class="birthday-celebrants-div">
                             @foreach($birthdays as $celebrant)
                             <div class="birthday-holder">
-                                <img src="{{ $employee->profile_img }}" class="img pull-left" alt="" style="width: 50px; height: 50px; margin-top: -10px; box-shadow: 1px 1px 10px 7px #fff; margin-right: 20px;">
+                                <img src="{{ $celebrant->profile_img }}" class="img pull-left" alt="" style="width: 50px; margin-top: -10px; box-shadow: 1px 1px 10px 7px #fff; margin-right: 20px;">
                                 <p><a target="_blank" href="employee_info/{{$celebrant->id}}" target="">{{ $celebrant->fullname() }}</a><br><span ><span class="fa fa-gift"></span> {{ monthDay($celebrant->birth_date) }}</span></p> 
                             </div>
                             @endforeach

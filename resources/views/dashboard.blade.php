5fd63eb436757c9df5f2a7b664267151263a4454
John Manuel Derecho <jmanuel.derecho@gmail.com>
2018-04-06 05:56:25 +0800

    fix grammar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -105,7 +105,7 @@ Dashboard
         <div class="col-md-4">
             <div class="panel panel-default ">
                 <div class="panel-heading">
-                    Birthday Celebrants of {{ date('F') }} 
+                    Birthday Celebrants for {{ date('F') }} 
                     <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
                 <div class="panel-body timeline-container" >
                     @if(count($birthdays) > 0)

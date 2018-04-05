fdb6a2b4e13957f6584b6cdee619fc12bc7fa79b
John Manuel Derecho <jmanuel.derecho@gmail.com>
2018-04-06 06:02:30 +0800

    fix ui on employee view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@ Home
         <div class="col-md-4">
             <div class="panel panel-default ">
                 <div class="panel-heading">
-                    Birthday Celebrants of {{ date('F') }} 
+                    Birthday Celebrants for {{ date('F') }} 
                     <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
                 <div class="panel-body timeline-container" >
                     @if(count($birthdays) > 0)
@@ -46,7 +46,7 @@ Home
                         <div class="birthday-celebrants-div">
                             @foreach($birthdays as $celebrant)
                             <div class="birthday-holder">
-                                <img src="{{ $employee->profile_img }}" class="img pull-left" alt="" style="width: 50px; height: 50px; margin-top: -10px; box-shadow: 1px 1px 10px 7px #fff; margin-right: 20px;">
+                                <img src="{{ $employee->profile_img }}" class="img pull-left" alt="" style="width: 50px; height: 50px; margin-right: 20px;">
                                 <p><a target="_blank" href="employee_info/{{$celebrant->id}}" target="">{{ $celebrant->fullname() }}</a><br><span ><span class="fa fa-gift"></span> {{ monthDay($celebrant->birth_date) }}</span></p> 
                             </div>
                             @endforeach

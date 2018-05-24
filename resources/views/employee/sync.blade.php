223f14d662ea0888102d4b11049d04c1b37a9f26
John Manuel Derecho <jmanuel.derecho@gmail.com>
2018-05-24 21:30:40 +0800

    sync cron

--- resources/views/employee/sync.blade.php
+++ resources/views/employee/sync.blade.php
@@ -16,6 +16,9 @@ Employee / Import
     #bb input[type=file]{
         display: none;
     }
+    #result_messaging_div{
+        display: none;
+    }
 </style>
     <div class="col-md-4">
         <div class="section-header">
@@ -27,6 +30,37 @@ Employee / Import
                     <button id="sync_now" class="btn btn-success pull-right" style="background-color: #388E3C">
                         Sync Now
                     </button>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8" id="result_messaging_div">
+        <div class="section-header">
+            <h4>Sync Result</h4>
+        </div>
+        <div class="panel panel-container">
+            <div class="panel-body">
+                <div class="col-md-6" style="padding: 0px !important;">
+                    <p>Deleted Employees</p>
+                    <a type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Button with data-target
+                      </a>
+                    <div class="collapse in" id="collapseExample">
+                      <div class="card card-body" id="deleted_employees_div">
+
+                      </div>
+                    </div>
+                </div>
+                <div class="col-md-6" style="padding: 0px !important;">
+                    <p>Inserted Employees</p>
+                    <a  type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Button with data-target
+                      </a>
+                    <div class="collapse in" id="collapseExample">
+                      <div class="card card-body" id="inserted_employee_div">
+
+                      </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -44,25 +78,35 @@ Employee / Import
     function checkSuccess(){
         if(cronimport && cronattrition) {
             alert("success");
+            for (var i = 0; i < attrition_result.deleted ; i++) {
+                $('#deleted_employees_div').append('<p>' + attrition_result.deleted[i] + '</p>');
+            }
+            // for (var i = 0; i < import_result.deleted ; i++) {
+            //     $('#inserted_employee_div').append('<p>' + import_result.inserted[i] + '</p>');
+            // }
+            $('#result_messaging_div').show();
         }
     }
     $('#sync_now').click(function(){
-        $.ajax({url: "{{ url('/cron/attrition') }}", success: function(result){
+        cronimport = false;
+        cronattrition = false;
+        $.ajax({url: "{{ url('/cron/importlatest') }}", success: function(result){
             import_result = result;
+            console.log(result);
+            cronimport = true;
+            checkSuccess();
+            },
+        dataType: "json"
+        });
+
+        $.ajax({url: "{{ url('/cron/attrition') }}", success: function(result){
+            attrition_result = result;
               console.log(result);
               cronattrition = true;
               checkSuccess();
               console.log();
             },
             dataType: "json"
-        });
-        $.ajax({url: "{{ url('/cron/importlatest') }}", success: function(result){
-            attrition_result = result;
-            console.log(result);
-            cronimport = true;
-            checkSuccess();
-            },
-        dataType: "json"
         });
     });
 </script>

4f453063cb4afcf1c87df904c1d3526771e73ef4
John Manuel Derecho <jmanuel.derecho@gmail.com>
2019-08-23 00:24:24 +0800

    update

--- app/ElinkActivities.php
+++ app/ElinkActivities.php
@@ -10,6 +10,6 @@ class ElinkActivities extends Model
     protected $table = "elink_activities";
 
     public function scopeThisMonth($query){
-    	return $query->whereRaw('MONTH(activity_date) =' . date('n') . " AND YEAR(activity_date) = " .date('Y') . 'OR MONTH(activity_date) = ' . date("n", strtotime("first day of previous month")) . " AND YEAR(activity_date) = " . date("Y", strtotime("first day of previous month")));
+    	return $query->whereRaw('MONTH(activity_date) =' . date('n') . " AND YEAR(activity_date) = " .date('Y') . ' OR MONTH(activity_date) = ' . date("n", strtotime("first day of previous month")) . " AND YEAR(activity_date) = " . date("Y", strtotime("first day of previous month")));
     }
 }

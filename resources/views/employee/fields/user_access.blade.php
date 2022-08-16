de1dd4dd5cb472f429359b533fd05dcee20d9666
June Vic Cadayona <jvcadz@gmail.com>
2022-08-17 03:04:43 +0800

    Adjust UI in linkees

--- resources/views/employee/fields/user_access.blade.php
+++ resources/views/employee/fields/user_access.blade.php
@@ -59,14 +59,14 @@
             <div class="form-group">
             
                 <label>Additional Linkees</label>
-                <div class="my-2 d-flex gap-2  p-2" style="width: 100%;flex-wrap: wrap;" id="linkees">
+                <div class="my-2 d-flex gap-2 p-2" style="width: 100%;flex-wrap: wrap;" id="linkees">
                     @foreach ($linkees as $linkee)
-                        <div class="border border-success rounded-pill p-2" id="linkee-{{$linkee->id}}" style="font-size: 12px; min-width:100px;">
+                        <div class="border border-success rounded-pill p-1" id="linkee-{{$linkee->id}}" style="font-size: 12px; min-width:100px;">
                             <input type="hidden" name="linkee-{{$linkee->id}}" value="{{$linkee->id}}">
-                            <span>{{$linkee->first_name}} {{$linkee->last_name}}</span>
+                            <span class="ms-2">{{$linkee->first_name}} {{$linkee->last_name}}</span>
                             <button type="button" class="btn btn-sm" onclick="deleteNodeAndData(document.getElementById('linkee-{{$linkee->id}}'))">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                   </svg>
                             </button>
                         </div>
